0d068c93894bc4e62acb7b5817e4000d720d1ad7
Igor Tciva <homspage@gmail.com>
2018-01-19 16:01:34 +0300

    Fixes

--- src/app.js
+++ src/app.js
@@ -2,18 +2,21 @@
 
 import './app.scss';
 
-function Request(phone, type = 'не указан', category = 'не указана', size = 'не указано') {
+function Request(phone = 'телефон не указан', type = 'вид не указан', category = 'категория объекта не указана', size = 'площадь не указана') {
   const formdata = new FormData();
   formdata.append('type', type);
   formdata.append('phone', phone);
-  formdata.append('type', category);
-  formdata.append('phone', size);
+  formdata.append('category', category);
+  formdata.append('size', size);
   const url = 'mail.php';
   const options = { 
     method: 'POST',
     body: formdata
   };
-
+  console.log(formdata.get('phone'));
+  console.log(formdata.get('type'));
+  console.log(formdata.get('category'));
+  console.log(formdata.get('size'));
   fetch(url, options)
     .then(res => console.log(res))
     .catch(er => console.log(er));
@@ -62,7 +65,9 @@ function onDropdownClick(e) {
   const elements = document.querySelectorAll('.choose-the-city__select>div');
   if (this.hide) {
     elements.forEach(el => {
+      this.classList.add('opened');
       el.classList.remove('hidden');
+      el.classList.add('opened-element');
     });
     this.hide = false;
   }
@@ -78,6 +83,8 @@ function onDropdownClick(e) {
     elements.forEach(el => {
       if (!el.classList.contains('selected')) {
         el.classList.add('hidden');
+        el.classList.remove('opened-element');
+        this.classList.remove('opened');
       }
     });
     this.hide = true;
@@ -88,7 +95,9 @@ function onDropdownClick(e) {
 // Действия при закрытии стартового окна с выбором города
 /////////////////////////////////////////////////////////
 const dropdownCloseBtn = document.querySelector('.choose-the-city__close');
+const dropdownOkBtn = document.querySelector('.choose-the-city__ok');
 dropdownCloseBtn.addEventListener('click', onCloseClick);
+dropdownOkBtn.addEventListener('click', onCloseClick);
 
 function onCloseClick(e) {
   const city = dropdown.querySelector('.selected').dataset.city;
@@ -107,6 +116,7 @@ function setDataFromCity(city) {
       case 'Киреевск':
         tel.textContent = '+7 (487) 263 67 17';
         contactTel.textContent = '+7 (487) 263 67 17';
+        contactTel.classList.remove('hidden');
         break;
       case 'Новомосковск':
       case 'Донской':
@@ -120,17 +130,20 @@ function setDataFromCity(city) {
       case 'Орел':
       case 'Калуга':
         tel.textContent = '+7 (800) 505 49 06';
-        contactTel.textContent = '+7 (800) 505 49 06';
+        // contactTel.textContent = '+7 (800) 505 49 06';
+        contactTel.classList.add('hidden');
         break;
       case 'Рязань':
         tel.textContent = '+7 (491) 242 71 22';
         contactTel.textContent = '+7 (491) 242 71 22';
+        contactTel.classList.remove('hidden');
         break;
       case 'Москва':
       case 'Моск. Обл.':
         tel.textContent = '+7 (499) 700 10 55';
         contactTel.textContent = '+7 (499) 700 10 55';
         contactAdress.textContent = 'г. Москва, октябрьская, 33';
+        contactTel.classList.remove('hidden');
         break;
     }
 
@@ -138,7 +151,7 @@ function setDataFromCity(city) {
       msk: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2243.2459887661303!2d37.61096821626544!3d55.78896638056302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54a05fc23869d%3A0x4d9372252746cb32!2z0J7QutGC0Y_QsdGA0YzRgdC60LDRjyDRg9C7LiwgMzMsINCc0L7RgdC60LLQsCwgMTI3MDE4!5e0!3m2!1sru!2sru!4v1516279697383" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
       tyla: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2334.6393604630525!2d37.59063171621754!3d54.18646258016358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x413440769a06c331%3A0xe354974309eb0032!2z0YPQuy4g0KTRgNGD0L3Qt9C1LCA0Miwg0KLRg9C70LAsINCi0YPQu9GM0YHQutCw0Y8g0L7QsdC7LiwgMzAwMDM0!5e0!3m2!1sru!2sru!4v1516280023790" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
       aliksin: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2317.028274743489!2d37.06479381622672!3d54.49772478023651!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4134f0df75e7c90b%3A0x5354eab5eded279!2z0YPQuy4g0JvQtdC90LjQvdCwLCAyMiwg0JDQu9C10LrRgdC40L0sINCi0YPQu9GM0YHQutCw0Y8g0L7QsdC7LiwgMzAxMzYy!5e0!3m2!1sru!2sru!4v1516280145559" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
-      nvmsk: '',
+      nvmsk: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2346.3794190049452!2d38.3075473162113!3d53.97828718011631!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41369967f3bf7401%3A0x1f934ca735c93189!2z0J3QvtCy0L7QvNC-0YHQutC-0LLRgdC60LDRjyDRg9C7LiwgNTgsINCU0L7QvdGB0LrQvtC5LCDQotGD0LvRjNGB0LrQsNGPINC-0LHQuy4sIDMwMTc3Mw!5e0!3m2!1sru!2sru!4v1516361305705" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
       zhelenogorsk: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.6282705402023!2d35.36121051616328!3d52.340889479780735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x412dd75cc048f745%3A0xb4984b0c289c09d8!2z0YPQuy4g0JPQsNCz0LDRgNC40L3QsCwgMTEsINCW0LXQu9C10LfQvdC-0LPQvtGA0YHQuiwg0JrRg9GA0YHQutCw0Y8g0L7QsdC7LiwgMzA3MTcx!5e0!3m2!1sru!2sru!4v1516280191400" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
       bransk: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2387.3530267365513!2d34.351438316189686!3d53.24737297995838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x412d585c184dc599%3A0xbe2b4bdb6c0b1bee!2z0YPQuy4g0KHQvtCy0LXRgtGB0LrQsNGPLCAxNywg0JHRgNGP0L3RgdC6LCDQkdGA0Y_QvdGB0LrQsNGPINC-0LHQuy4sIDI0MTA1MA!5e0!3m2!1sru!2sru!4v1516280226725" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
       orel: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2403.4995702975098!2d36.062212216181265!3d52.9574269798994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41321a77bbe42329%3A0xa03b8eeb6451a45c!2z0YPQuy4g0JrQvtC80YHQvtC80L7Qu9GM0YHQutCw0Y8sIDUwLCDQntGA0ZHQuywg0J7RgNC70L7QstGB0LrQsNGPINC-0LHQuy4sIDMwMjAwMQ!5e0!3m2!1sru!2sru!4v1516280270877" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
@@ -158,7 +171,7 @@ function setDataFromCity(city) {
       case 'Донской':
       case 'Узловая':
       case 'Кимовск':
-        contactAdress.textContent = 'г. Новомосковск, Новомосковская, 58';
+        contactAdress.textContent = 'г. Донской, Новомосковская, 58';
         contactMap.innerHTML = maps.nvmsk;
         break;
       case 'Алексин':
@@ -295,6 +308,7 @@ calcSelects.forEach(el => {
 
 function onCalcNextBtnClick(e) {
   e.preventDefault();
+  const progress = document.querySelector('.calculator__progress');
 
   if (step < 4) {
     calcControlls.forEach(el => {
@@ -308,6 +322,7 @@ function onCalcNextBtnClick(e) {
         document.querySelector('.category').classList.remove('hidden');
         calcStepName.textContent = steps[1];
         calcProgress.style.width = '20%';
+        progress.textContent = '25%';
         step ++;
         break;
       case 1:
@@ -322,6 +337,7 @@ function onCalcNextBtnClick(e) {
           calcStepName.textContent += ('(Сот)');
         if (calcInfo.category === 'Юр. Лицо' || calcInfo.category === 'Дом')
           calcStepName.textContent += ('(м)');
+          progress.textContent = '50%';
         step ++;
         break;
       case 2:
@@ -335,6 +351,7 @@ function onCalcNextBtnClick(e) {
         document.querySelector('.phone').classList.remove('hidden');
         calcStepName.textContent = steps[4];
         calcProgress.style.width = '65%';
+        progress.textContent = '75%';
         step ++;
         calcStepName.textContent += calcPrice() + 'р';
         break;
@@ -343,6 +360,7 @@ function onCalcNextBtnClick(e) {
                                 .querySelector('.calculator__input').value;
         calcStepName.textContent = steps[5];
         calcProgress.style.width = '85%';
+        progress.textContent = '100%';
         break;
     }
 
@@ -360,6 +378,7 @@ function onCalcNextBtnClick(e) {
 
 function resetStep(e) {
   e.preventDefault();
+  document.querySelector('.calculator__progress').textContent = '0%';
   step = 0;
   calcInfo.insec = '';
   calcInfo.category = '';
@@ -372,6 +391,17 @@ function resetStep(e) {
   calcStep.textContent = step + 1;
   calcStepName.textContent = steps[0];
   calcProgress.style.width = '0.7%';
+
+  const calcSelects = document.querySelector('.calculator').querySelectorAll('.calculator__select');
+  calcSelects.forEach(el => {
+    const opts = el.querySelectorAll('.calculator__option');
+    opts.forEach(elm => {
+      elm.classList.add('hidden');
+      elm.classList.remove('selected');
+    });
+    opts[0].classList.remove('hidden');
+    opts[0].classList.add('selected');
+  });
 }
 
 function toggleSelect(e) {
@@ -406,7 +436,7 @@ function calcPrice() {
   const category = calcInfo.category;
 
   if (category === 'Участок') {
-    if (size > 0 && size < 6) {
+    if (size < 6) {
       return 2500;
     }
     if (size === 6) {
@@ -552,4 +582,70 @@ function calcPrice() {
         }
     }
   }
+}
+
+/////////////////////////////////////////////////
+// Отзывы  //////////////////////////////////////
+/////////////////////////////////////////////////
+
+const prevRevBtn = document.querySelector('.rewiews__btn--prev');
+const nextRevBtn = document.querySelector('.rewiews__btn--next');
+const revTitle = document.querySelector('.rewiews__item__title');
+const revText = document.querySelector('.rewiews__item__text');
+const revDate = document.querySelector('.rewiews__item__data');
+
+prevRevBtn.addEventListener('click', onPrevRevClick);
+nextRevBtn.addEventListener('click', onNextRevClick);
+
+let currentRev = 0;
+const reviews = [
+  {
+    date: '25 ноября 2017',
+    name: 'Александр (г. Тула)',
+    text: 'Хотелось бы выразить особую признательность фирме «Центр Дезинсекции» за помощь в решении нашей проблемы. Живём с женой в двухкомнатной квартире уже много лет и мучаемся от регулярного нашествия тараканов в наш дом. Своими силами уничтожить вредных насекомых в нашем жилище не получалось, пока соседи не посоветовали обратиться к профессионалам. Ребята быстро приехали по звонку и решили нашу проблему за час. Огромное спасибо.'
+  },
+  {
+    date: '2 сентября 2017',
+    name: 'Алёна г. Рязань',
+    text: 'Вспоминая с ужасом время, когда приходилось лезть в погреб, где обитали крысы. Обратившись к вам, я не пожалела, сотрудники быстро уничтожили грызунов во всём подвале. Спасибо'
+  },
+  {
+    date: '25 декабря 2017',
+    name: 'Елена г. Новомосковск',
+    text: 'Недавно купили небольшую квартиру, но радовались не долго, через месяц стали замечать укусы на теле, ужасом осознали, что это клопы. Мама увидела в интернете вашу рекламу и решила позвонить. Всё получилось, отлично, клопов уничтожили и цены довольно низкие. Спасибо фирме, помогла, буду рекомендовать вас.'
+  },
+  {
+    date: '10 января 2018',
+    name: 'Ольга г. Москва',
+    text: 'Спасибо фирме «Центр Дезинсекции» за профессионализм и быстрое решение проблемы. Долго не могла избавиться от муравьёв, теперь остались только воспоминания. Молодцы!'
+  },
+  {
+    date: '7 августа 2017',
+    name: 'Дмитрий г. Венёв',
+    text: 'Хочу выразить огромную благодарность вашей фирме, вы меня сильно выручили, уничтожив грызунов на моём складе. Если честно, думал они опять вернуться и головной боли прибавится, видимо ошибался, вы качественно сделали свою работу, отдельное спасибо Алексею за консультацию. Обращусь обязательно если что.'
+  }
+]
+
+function onPrevRevClick(e) {
+  if (currentRev === 0)
+    currentRev = reviews.length - 1;
+  else
+    currentRev -- ;
+  
+    setRev();
+}
+
+function onNextRevClick(e) {
+  if (currentRev === reviews.length - 1)
+    currentRev = 0;
+  else
+    currentRev ++ ;
+
+    setRev();
+}
+
+function setRev() {
+  revTitle.textContent = reviews[currentRev].name;
+  revText.textContent = reviews[currentRev].text;
+  revDate.textContent = reviews[currentRev].date;
 }
